c04ea342830bbcd083f51134294d17f973c66e03
Victordong <zzdsakura@gmail.com>
2020-03-18 02:49:03 -0700

    noncopyable

---
+++ archer/base/noncopyable.hpp
@@ -0,0 +1,10 @@
+
+namespace archer {
+class noncopyable {
+   protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+};
+};  // namespace archer
