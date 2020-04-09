b30869eab6ad1ff827af443864032dfd4e9ebf5b
Victordong <zzdsakura@gmail.com>
2020-04-09 06:09:19 -0700

    finish buffer

--- archer/base/buffer.cpp
+++ archer/base/buffer.cpp
@@ -5,42 +5,131 @@ Buffer& Buffer::append(const char* p, size_t len) {
     if (len > space()) {
         malloc(std::max(2 * cap_, len - space()));
     }
-    if (write_pos_ + len > cap_) {
-        auto right = cap_ - write_pos_;
-        auto left = (write_pos_ + len) % cap_;
-        std::copy(p, p + right, data()+write_pos_);
-        std::copy(p + right, p + len, data());
-    } else {
-        std::copy(p, p + len, end());
-    }
-    write_pos_ = (write_pos_ + len) % cap_;
+    std::copy(p, p + len, end());
+    write_pos_ = write_pos_ + len;
     return *this;
 }
 
 Buffer& Buffer::malloc(size_t len) {
-    int new_size;
-    if (len == 0) {
-        if (new_size < kInitSize * kInitSize) {
-            new_size = cap_ * 2;
-        } else {
-            new_size = kInitSize * kInitSize + cap_;
-        }
+    if (space() + pre_space() > len) {
+        std::copy(begin(), end(), data());
     } else {
-        new_size = cap_ + len;
-    }
-
-    auto new_buf = std::vector<char>(new_size);
-    if(read_pos_>write_pos_) {
-        std::copy(begin(), data() + cap_, new_buf.data());
-        std::copy(data(), end(), new_buf.data() + cap_ - read_pos_);
-    } else {
+        auto new_size = cap_ + len;
+        auto new_buf = std::vector<char>(new_size);
         std::copy(begin(), end(), new_buf.data());
+        buf_ = std::move(new_buf);
+        cap_ = new_size;
     }
-    buf_ = std::move(new_buf);
-
-    write_pos_ = (write_pos_ - read_pos_ + cap_) % cap_;
+    write_pos_ = write_pos_ - read_pos_;
     read_pos_ = 0;
-    cap_ = new_size;
-
     return *this;
+}
+
+void Buffer::copyFrom(const Buffer& buf) {
+    if (this != &buf) {
+        buf_ = buf.buf_;
+        cap_ = buf.cap_;
+        write_pos_ = buf.write_pos_;
+        read_pos_ = buf.read_pos_;
+    }
+}
+
+void Buffer::moveFrom(Buffer&& buf) {
+    if (this != &buf) {
+        buf_ = std::move(buf.buf_);
+        cap_ = buf.cap_;
+        write_pos_ = buf.write_pos_;
+        read_pos_ = buf.read_pos_;
+    }
+}
+
+Buffer& Buffer::append(Buffer& buf) {
+    if (this != &buf) {
+        append(buf.data(), buf.size());
+    }
+    return *this;
+}
+
+inline Slice Slice::eatWord() {
+    auto b_iter = begin_;
+    while (b_iter < end_ && ::isspace(*b_iter)) {
+        b_iter++;
+    }
+    auto e_iter = b_iter;
+    while (e_iter < end_ && !::isspace(*e_iter)) {
+        e_iter++;
+    }
+    begin_ = e_iter;
+    return Slice(b_iter, e_iter);
+}
+
+inline Slice Slice::eatLine() {
+    auto slice_begin = begin_;
+    while (begin_ < end_ && *begin_ != '\r' && *begin_ != '\n') {
+        begin_++;
+    }
+    return Slice(slice_begin, begin_);
+}
+
+inline Slice& Slice::trimSpace() {
+    while (begin_ < end_ && ::isspace(*begin_)) {
+        begin_++;
+    }
+    while (end_ > begin_ && ::isspace(*end_)) {
+        end_--;
+    }
+    return *this;
+}
+
+inline int Slice::compare(const Slice& slice) const {
+    auto len = size(), s_len = slice.size();
+    auto min_len = std::min(len, s_len);
+    int r = ::memcmp(data(), slice.data(), min_len);
+    if (r == 0) {
+        if (len > s_len) {
+            return 1;
+        } else if (s_len > len) {
+            return -1;
+        }
+    }
+    return r;
+}
+
+inline bool operator<(const Slice& a, const Slice& b) {
+    return a.compare(b) < 0;
+}
+
+inline bool operator>(const Slice& a, const Slice& b) {
+    return a.compare(b) > 0;
+}
+
+inline bool operator>=(const Slice& a, const Slice& b) {
+    return a.compare(b) >= 0;
+}
+
+inline bool operator<=(const Slice& a, const Slice& b) {
+    return a.compare(b) <= 0;
+}
+
+inline bool operator==(const Slice& a, const Slice& b) {
+    return a.compare(b) == 0;
+}
+
+inline bool operator!=(const Slice& a, const Slice& b) {
+    return a.compare(b) != 0;
+}
+
+inline SliceList Slice::split(char c) const {
+    SliceList list;
+    auto slice_begin = begin_;
+    for (auto iter = begin_; iter < end_; iter++) {
+        if (*iter == c) {
+            list.push_back(Slice(slice_begin, iter));
+            slice_begin = iter + 1;
+        }
+    }
+    if (begin_ != end_) {
+        list.push_back(Slice(slice_begin, end_));
+    }
+    return list;
 }
--- archer/base/buffer.hpp
+++ archer/base/buffer.hpp
@@ -1,72 +1,107 @@
 #ifndef _ARCHER_BUFFER
 #define _ARCHER_BUFFER
 #include <stddef.h>
-#include <vector>
 #include <string.h>
+
+#include <string>
+#include <vector>
 
 namespace archer {
 static int kInitSize = 1024;
 
-class Slice {
-    public:
-     Slice(char*, size_t);
-     char* data();
-     size_t size();
+class Slice;
 
-    private:
-     char* slice_b_;
-     size_t size_;
+using SliceList = std::vector<Slice>;
+
+class Slice {
+   public:
+    Slice() : begin_(nullptr), end_(nullptr){};
+    Slice(const char* begin, const char* end) : begin_(begin), end_(end){};
+    Slice(std::string& s) : begin_(s.data()), end_(s.data() + s.size()){};
+    Slice(const char* begin, size_t len) : begin_(begin), end_(begin + len){};
+    Slice(const char* begin) : begin_(begin), end_(begin + strlen(begin)){};
+    ~Slice(){};
+
+    const char* begin() const { return begin_; };
+    const char* data() const { return begin_; };
+    const char* end() const { return end_; };
+    size_t size() const { return end_ - begin_; };
+    void resize(size_t len) { end_ = begin_ + len; };
+    inline bool empty() const { return begin_ == end_; };
+    inline char operator[](int i) { return *(begin_ + i); };
+    void clear() { begin_ = end_ = nullptr; };
+
+    Slice eatWord();
+    Slice eatLine();
+    Slice eat(size_t len) {
+        auto old_begin = begin_;
+        begin_ = begin_ + len;
+        return Slice(old_begin, len);
+    };
+
+    Slice sub(int boff, int eoff = 0) const {
+        return Slice(begin_ + boff, end_ - eoff);
+    };
+
+    Slice& trimSpace();
+
+    SliceList split(char c) const;
+
+    std::string toString() const { return std::string(begin_, end_); };
+
+    int compare(const Slice&) const;
+
+    bool startWith(const Slice& slice) {
+        return (size() >= slice.size()) &&
+               (::memcmp(data(), slice.data(), slice.size()) == 0);
+    };
+    bool endWith(const Slice& slice) {
+        return (size() >= slice.size()) &&
+               (::memcmp(end() - slice.size(), slice.data(), slice.size()) ==
+                0);
+    };
+    operator std::string() { return std::string(begin_, end_); };
+
+   private:
+    const char* begin_;
+    const char* end_;
 };
 
 class Buffer {
    public:
-    Buffer()
-        : buf_(kInitSize),
-          cap_(kInitSize),
-          write_pos_(0),
-          read_pos_(0),
-          empty_(true){};
+    Buffer() : buf_(kInitSize), cap_(kInitSize), write_pos_(0), read_pos_(0){};
 
     Buffer(const Buffer& buf) { copyFrom(buf); };
 
-    Buffer(Buffer&& buf);
+    Buffer(Buffer&& buf) { moveFrom(std::move(buf)); };
 
     ~Buffer() { buf_.clear(); };
 
     void clear() {
         write_pos_ = 0;
         read_pos_ = 0;
-        empty_ = true;
         buf_.clear();
     }
 
-    size_t size() const {
-        if(write_pos_-read_pos_!=0) {
-            return (write_pos_ - read_pos_ + cap_) % cap_;
-        } else {
-            if(empty_) {
-                return 0;
-            } else {
-                return cap_;
-            }
-        }
-    }
+    size_t size() const { return write_pos_ - read_pos_; };
 
-    bool empty() { return empty_; };
+    bool empty() { return write_pos_ - read_pos_ == cap_; };
 
-    bool full() { return !empty_ && read_pos_ == write_pos_; };
+    bool full() { return read_pos_ == write_pos_; };
 
     char* data() { return buf_.data(); };
+
+    char* cap() { return buf_.data() + cap_; };
 
     char* begin() { return buf_.data() + read_pos_; };
 
     char* end() { return buf_.data() + write_pos_; };
 
-    size_t space() const { return cap_ - size(); }
+    size_t space() const { return cap_ - write_pos_; };
+
+    size_t pre_space() const { return read_pos_; };
 
     Buffer& malloc(size_t len);
-
-    void malloc() {}
 
     Buffer& append(const char* p, size_t len);
 
@@ -74,29 +109,40 @@ class Buffer {
 
     Buffer& append(const char* p) { return append(p, sizeof(p)); };
 
-    Buffer& consume(size_t len) { 
+    Buffer& append(Buffer& buf);
+
+    Buffer& consume(size_t len) {
         read_pos_ += len;
-        if(size()==0) {
+        if (size() == 0) {
             clear();
         }
         return *this;
+    };  // shiftN
+
+    Buffer& operator=(const Buffer& buf) {
+        if (&buf == this) {
+            return *this;
+        }
+        copyFrom(buf);
+        return *this;
     };
 
-    Buffer& absorb(Buffer& buf);
-
-    Buffer& operator=(const Buffer& buf) { return *this; };
-
-    Buffer& operator=(Buffer&& buf) { return *this; };
+    Buffer& operator=(Buffer&& buf) {
+        if (&buf == this) {
+            return *this;
+        }
+        moveFrom(std::move(buf));
+        return *this;
+    };
 
     operator Slice() { return Slice(begin(), size()); };
 
    private:
     std::vector<char> buf_;
     size_t cap_, write_pos_, read_pos_;
-    bool empty_;
 
-    void expand(size_t len);
-    void copyFrom(const Buffer& b);
+    void copyFrom(const Buffer& buf);
+    void moveFrom(Buffer&& buf);
 };
 
 };  // namespace archer
